faa17c2569ed6cc176602d78c923daf08193410c
rf922 <rfabiani@sfsu.edu>
2025-01-20 13:49:48 -0800

    Move Gradient Descent logic to util file

--- src/LineGui/util/GradientDescentUtil.java
+++ src/LineGui/util/GradientDescentUtil.java
@@ -137,28 +137,28 @@ public class GradientDescentUtil {
     /**
      * Converts screen X coordinate to model X coordinate.
      */
-    public double toModelX(double screenX) {
+    private double toModelX(double screenX) {
         return screenX / pane.getWidth() * 2 - 1;
     }
 
     /**
      * Converts screen Y coordinate to model Y coordinate.
      */
-    public double toModelY(double screenY) {
+    private double toModelY(double screenY) {
         return 1 - screenY / pane.getHeight() * 2;
     }
 
     /**
      * Converts model X coordinate to screen X coordinate.
      */
-    public double toScreenX(double modelX) {
+    private double toScreenX(double modelX) {
         return (modelX + 1) / 2 * pane.getWidth();
     }
 
     /**
      * Converts model Y coordinate to screen Y coordinate.
      */
-    public double toScreenY(double modelY) {
+    private double toScreenY(double modelY) {
         return (1 - modelY) / 2 * pane.getHeight();
     }
 
